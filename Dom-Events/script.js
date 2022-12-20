7f05525f206137a280c5e22b082953f3ebe8555c
sanjay <sanjayprashanth1997@gmail.com>
2022-12-20 10:54:10 +0530

    new_dom_files

---
+++ Dom-Events/script.js
@@ -0,0 +1,44 @@
+
+for (let i=0; i<6; i++){
+   
+  console.log(i);
+}
+console.log(window);
+
+
+function newFunction(){
+    console.log("hey i'm the function");
+    alert("hi there")
+}
+
+function newSecons(){
+    console.log("hey i'm the new function");
+
+}
+
+setTimeout(newSecons, 5000); 
+
+
+
+
+
+// alert 
+// prompt
+// confirm
+
+
+
+// minimum js code -> empty js file
+
+ // window -> document 
+
+ // sample function
+//  const log = () => {
+//     console.log("hi there"); 
+//  }
+
+//  const windwObject = {
+//     name : log(),
+//  }
+
+// windwObject.name; 
