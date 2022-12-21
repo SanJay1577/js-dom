92dd92634719096997699e1b603b0606a7396dec
sanjay <sanjayprashanth1997@gmail.com>
2022-12-21 10:50:08 +0530

    new-content

--- Dom-Events/script.js
+++ Dom-Events/script.js
@@ -1,44 +1,90 @@
 
-for (let i=0; i<6; i++){
-   
-  console.log(i);
-}
-console.log(window);
 
 
-function newFunction(){
-    console.log("hey i'm the function");
-    alert("hi there")
-}
 
-function newSecons(){
-    console.log("hey i'm the new function");
 
+
+
+// alert // prompt //confirm
+
+//////////alert ////////////////
+
+function alertFunction(message) {
+  alert(message); // nothing
 }
 
-setTimeout(newSecons, 5000); 
+/////////promt////////////////
+let promptElement = document.createElement("div")
+document.body.append(promptElement); 
+
+function promptFunction(){
+  let answer = prompt("Hey there have you attended the class?", "") // returns a null or value
+ 
+    promptElement.textContent = `${answer} is your answer`
+  
+}
+
+//////////confrim////////////////////
+
+function confirmFunction(){
+  let response = confirm("Are you confirming?"); // true or false
+
+  if (response == true){
+    promptElement.textContent = `You have approved the request`
+  } else {
+    promptElement.textContent = `You have rejected the request`
+  }
+}
+
+/////////set time out////////////////
+
+let count = 10; 
+
+function timeOutExample(){
+   // count++ or count = count +1 
+  promptElement.textContent = count; 
+  count = count-1;
+  const id = setTimeout(timeOutExample, 1000); 
+  if (count == 0){
+    clearTimeout(id); 
+    promptElement.textContent = "Your time is up..."
+  }
+}
 
 
+///////////set interval//////////////
+// repeat after certain time 
+function intervalFunction(){
+let ourInterval = setInterval(() => {
+   promptElement.textContent = count
+   count = count - 1
+   if (count === 0 ){
+    clearInterval(ourInterval)
+    promptElement.textContent = "your time has stopped..."
+   }
+}, 1000);
+}
+
+/////////Events////////
+let clickCounter = 0
+
+function clickEvent(){
+  clickCounter++;
+  console.log("You clicked this", clickCounter)
+ const inputVarible = document.getElementById("input-id"); 
+ console.log(inputVarible.value);
+ inputVarible.value = "";
+ 
+}
+let changeCounter = 0
+function changeEvent(){
+  changeCounter++;
+  console.log("You changed this", changeCounter)
+}
 
 
-
-// alert 
-// prompt
-// confirm
-
-
-
-// minimum js code -> empty js file
-
- // window -> document 
-
- // sample function
-//  const log = () => {
-//     console.log("hi there"); 
-//  }
-
-//  const windwObject = {
-//     name : log(),
-//  }
-
-// windwObject.name; 
+let addBtn = document.getElementById("btn-id")
+/////
+addBtn.addEventListener("click", ()=>{
+  console.log("you have created a function")
+})
