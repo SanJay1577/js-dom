5bf16dc911fc0d4f04759401d07d73b93336a136
sanjay <sanjayprashanth1997@gmail.com>
2022-12-28 10:56:07 +0530

    fetch_Data

--- Async-events/promises.js
+++ Async-events/promises.js
@@ -17,9 +17,11 @@ let promise = new Promise(
 
 let newPromise = new Promise((resolve, reject)=>{
     setTimeout(()=>{
+        //console.log("Priomise 1 is emiited and resolved")
        resolve(25)
-    }, 5000)
+    }, 1000)
 })
+
 
 // consumer of promise
 promise
@@ -41,10 +43,103 @@ newPromise
 }).then((val3)=>{
     console.log(val3);
 })
- 
- 
+
+//promise all (multiple promises)
 
 
+let promise1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("1st promise is called")
+       // resolve(40)
+      reject("rejected your promise at 1")
+    }, 4000)
+})
+
+let promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("2nd promise is called")
+        resolve(25)
+        //reject("rejected your promise at 2")
+    }, 7000)
+})
+
+
+let promise3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("3rd promise is called")
+        resolve(50)
+        //reject("rejected your promise at 3")
+    }, 9000)
+})
+ 
+
+ Promise.all([promise1, promise2, promise3])
+ .then((value)=>{
+   console.log(`The value is : `,value)
+ }).catch((err)=>{
+    console.log(`You have an error :`, err)
+ }); 
+
+//callbacks and promises 
+
+function firstReturnFunction(outValue) {
+    console.log(`the out value is ${outValue}`)
+
+}
+
+// function main(name) {
+//     console.log(`name : ${name}`)
+//   return name;
+// }
+
+// function firstReturnFunction(outValue) {
+//     console.log(`the out value is ${outValue}`)
+// }
+
+ 
+// fetch ----------
+//xml parseFloat(`xml data`)
+
+//API -> Address
+
+fetch("https://restcountries.com/v3.1/all")
+.then((response)=>response.json())
+.then((data)=> {
+      console.log(data);
+
+      data.forEach(element => {
+     const countryRequired = {
+        ...element, 
+        name: element.name.common,
+        flag : element.flags.png,
+        population: element.population,
+        region : element.region,
+        capital : element.capital
+     }
+      createCountry(countryRequired)
+     });
+
+})
+.catch((err)=>console.log(err));
+
+function createCountry({name, flag, population, region, capital}) {
+    document.body.innerHTML += `
+     <div class = "container">
+      <img src= "${flag}" alt="${name}" class="flag" />
+      <div class= "info">
+       <h2>${name}</h2>
+       <p><span>Population : </span>${population}</p>
+       <p><span>Region : </span>${region}</p>
+       <p><span>Capital : </span>${capital}</p>
+      </div>
+     </div>
+       `
+}
+
+// flag
+// population
+// region
+//capital-name
 
 
 // pormise = {} Object
