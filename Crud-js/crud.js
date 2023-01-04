45cd255d639d6af0daf96eee51afa95af8f72dc0
sanjay <sanjayprashanth1997@gmail.com>
2023-01-04 09:18:31 +0530

    array and object data

--- Crud-js/crud.js
+++ Crud-js/crud.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const addBtn = document.querySelector("#new-stud-btn"); 
-    const studentForm = document.querySelector(".add-student-form"); 
-    const studentList = document.querySelector("#student-list"); 
-    let addStudent = false;
-    const API_URL = "http://localhost:3000/students"; 
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-stud-btn");
+  const studentForm = document.querySelector(".add-student-form");
+  const studentList = document.querySelector("#student-list");
+  let addStudent = false;
+  const API_URL = "http://localhost:3000/students";
 
-// This will render a individual student card
-function renderStudents(stud) {
-        const studDiv = document.createElement("div"); 
-        studDiv.className = "card";
-        studDiv.innerHTML += `
+  // This will render a individual student card
+  function renderStudents(stud) {
+    const studDiv = document.createElement("div");
+    studDiv.className = "card";
+    studDiv.innerHTML += `
       <img src= "${stud.image}" alt="${stud.name}" class="stud-avatar" />
        <h2>${stud.name}</h2>
        <p><span>class : </span>${stud.class}</p>
@@ -17,109 +17,128 @@ function renderStudents(stud) {
        <p>${stud.like}</p>
        <button data-id =${stud.id} class="like-btn">Likes</button>
        <button data-id ='${stud.id}' class="delete-btn">Delete</button>
-      `
+      `;
     studentList.appendChild(studDiv);
-}
+  }
 
-// the below function will loop through all the student details
-// and render the student card function
-function renderAllStudents (students) {
-    studentList.innerHTML ="";
-    students.forEach(student => {
-        renderStudents(student); 
-    });   
-}
-// fetching the api for student details 
-fetch(API_URL, {
-    method:"GET"
-})
-.then((response)=>response.json())
-.then((data)=>renderAllStudents(data))
+  // the below function will loop through all the student details
+  // and render the student card function
+  function renderAllStudents(students) {
+    studentList.innerHTML = "";
+    students.forEach((student) => {
+      renderStudents(student);
+    });
+  }
+  // fetching the api for student details
+  fetch(API_URL, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => renderAllStudents(data));
 
-
-
-addBtn.addEventListener("click", ()=>{
-    // change the variable to true and false 
+  addBtn.addEventListener("click", () => {
+    // change the variable to true and false
     //false -> true
-    addStudent = !addStudent
+    addStudent = !addStudent;
 
-   if(addStudent) {
-    studentForm.style.display = "block"
-    addBtn.textContent = "Add and close"
-   } else {
-    studentForm.style.display = "none"
-    addBtn.textContent = "Add Student"
-   }
-})
-// adding a data
-studentForm.addEventListener("click", (event)=>{
-    let studentName = document.getElementsByClassName("input-text")[0].value
-    let studentImage = document.getElementsByClassName("input-text")[1].value
-    let studentClass = document.getElementsByClassName("input-text")[2].value
-    let studentCountry = document.getElementsByClassName("input-text")[3].value
-   
-   let data = {
+    if (addStudent) {
+      studentForm.style.display = "block";
+      addBtn.textContent = "Add and close";
+    } else {
+      studentForm.style.display = "none";
+      addBtn.textContent = "Add Student";
+    }
+  });
+  // adding a data
+  studentForm.addEventListener("click", (event) => {
+    let studentName = document.getElementsByClassName("input-text")[0].value;
+    let studentImage = document.getElementsByClassName("input-text")[1].value;
+    let studentClass = document.getElementsByClassName("input-text")[2].value;
+    let studentCountry = document.getElementsByClassName("input-text")[3].value;
+
+    let data = {
       name: studentName,
       image: studentImage,
-      class : studentClass,
-      country : studentCountry
-    }
+      class: studentClass,
+      country: studentCountry,
+    };
     event.preventDefault();
-    if(event.target.name === "submit"){
-        // api call post 
-        fetch(API_URL, {
-            method:"POST",
-            body:JSON.stringify(data),
-            headers:{
-                "Content-Type":"application/json"
-            }
-        })
-        .then((response)=>response.json())
-        .then((student)=>renderStudents(student))
-        .catch((err)=>console.log(err))
-    }
-}); 
-
-
-// target the student list 
-// you have delete it
-
-studentList.addEventListener("click", (event)=>{
-    event.preventDefault();
-  if(event.target.className ==="like-btn") {
-    let id = event.target.dataset.id
-    let likes = parseInt(event.target.previousElementSibling.innerHTML)
-    likes++;
-    event.target.previousElementSibling.innerText = `${likes}`
-    let data = {
-        like:likes
-    }
-
-    fetch(`${API_URL}/${id}`, {
-        method :"PATCH",
-        body:JSON.stringify(data),
-        headers :{
-            "Content-Type":"application/json"
-        }
-    })
-
-  }
-  else if (event.target.className === "delete-btn"){
-
-     let id = event.target.dataset.id
-     let parent = event.target.parentNode
-     // delete functionality in fetch
-     fetch(`${API_URL}/${id}`, {
-        method:"DELETE", 
-        headers:{
-            "Content-Type":"application/json"
+    if (event.target.name === "submit") {
+      // api call post
+      fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
         },
-     })
-     .then(()=>parent.remove())
-     .catch((err)=>console.log(err))
-  } 
-  console.log("finished")
-})
+      })
+        .then((response) => response.json())
+        .then((student) => renderStudents(student))
+        .catch((err) => console.log(err));
+    }
+  });
 
+  // target the student list
+  // you have delete it
+  studentList.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.className === "like-btn") {
+      let id = event.target.dataset.id;
+      let likes = parseInt(event.target.previousElementSibling.innerHTML);
+      likes++;
+      event.target.previousElementSibling.innerText = `${likes}`;
+      let data = {
+        like: likes,
+      };
 
-})
+      fetch(`${API_URL}/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else if (event.target.className === "delete-btn") {
+      let id = event.target.dataset.id;
+      let parent = event.target.parentNode;
+      // delete functionality in fetch
+      fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(() => parent.remove())
+        .catch((err) => console.log(err));
+    }
+    console.log("finished");
+  });
+});
+
+/*
+Day-1 :
+mail 
+- Api 
+- 24 hours (10)
+- - querrys (9-7)
+- - github (push---- , netlify----)
+  - project 
+ index.html
+ script.js (DOM) & async and await (try and catch)
+  -- bootstrap
+  --style.css (optional)
+ --- anotations 
+    function names 
+    variable declation 
+    dry (don't repeat yourself) 
+--- comments 
+
+Day-2 :
+Mock Interview 
+15 - 30(min)
+html-css-js(basic)-js(advanced-dom, callback, promises, asyn and await , fetch)
+project-questions
+*/
+
+//5 -> backtracked 
+// -> different batch 
--- Query/script.js
+++ Query/script.js
@@ -1,30 +1,59 @@
-console.log("new script is working")
+console.log("new script is working");
 
 // first step is done......
-let ele = document.createElement("div"); 
+let ele = document.createElement("div");
 
-
-ele.textContent = "Hi I'm the new element"
-ele.style.color= "red"
-ele.setAttribute("id", "new-div")
-
+ele.textContent = "Hi I'm the new element";
+ele.style.color = "red";
+ele.setAttribute("id", "new-div");
 
 // second appending it
- document.body.append(ele);
+document.body.append(ele);
 
+//accessing the element
+let head = document.getElementById("heading");
 
- //accessing the element 
- let head = document.getElementById("heading"); 
+head.setAttribute("class", "head-class");
 
- head.setAttribute("class", "head-class")
+head.textContent = "i'm changed";
 
+function newFunction() {
+  console.log("new function");
+}
 
- head.textContent = "i'm changed"
+newFunction();
 
- function newFunction(){
-   console.log("new function");
- }
+//foreach // read
 
- newFunction();
+// map  // write
 
+const arr = [
+  { 
+    students:  [
+        {
+          firstname:"sanjay",
+          lastname :"guvi"
+        },
+        {
+          firstname : "arun",
+          lastname :"praveen"
+        }
+         ]
+   }, 
+   {
+   batch:"b42wd"
+   }
+];
 
+const array = [5, 7, 8, 9];
+
+array.map((item, id) => {
+  console.log(`The item is ${item}
+               The index is ${id}`);
+});
+
+console.log(arr[0].students[0].firstname);
+
+const filterdvalue = array.filter((item, id)=> id != 3); 
+console.log(filterdvalue);
+console.log(array);
